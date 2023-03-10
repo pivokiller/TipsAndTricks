1cc7b5baed230ffd91261f9bac887db896393ab1
pivokiller <2011353@dlu.edu.vn>
2023-03-11 00:30:48 +0700

    Classroom ex

---
+++ TatBlog.Services/Blogs/IAuthorResponsitory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Constracts;
+using TatBlog.Core.DTO;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Services.Blogs
+{
+    public interface IAuthorResponsitory
+    {
+
+        Task<Author> FindAuthorBySlugAsync(string slug, CancellationToken cancellationToken);
+
+        Task<Author> FindAuthorByIdAsync(int id, CancellationToken cancellationToken);
+
+        Task<IPagedList<AuthorItem>> GetPagedAuthorsAsync(
+           IPagingParams pagingParams, CancellationToken cancellationToken = default
+           );
+
+        Task<Author> UpdateAuthorAsync(Author author, CancellationToken cancellationToken = default);
+
+        Task<IPagedList<Author>> GetAuthorTopPostAsync(int n, IPagingParams pagingParams, CancellationToken cancellationToken = default);
+
+        Task<bool> IsAuthorExistBySlugAsync(int id, string slug, CancellationToken cancellationToken);
+    }
+}
--- TatBlog.Services/Blogs/IBlogRepository.cs
+++ TatBlog.Services/Blogs/IBlogRepository.cs
@@ -31,4 +31,28 @@ public interface IBlogRepository
     // Lấy danh sách từ khóa/thẻ và phân trang theo
     // các tham số pagingParams
     Task<IPagedList<TagItem>> GetPagedTagsAsync(IPagingParams pagingParams, CancellationToken cancellationToken = default);
+
+    Task<Tag> GetTagSlusAsync(string slug, CancellationToken cancellation = default);
+
+    Task<IList<TagItem>> GetTagsAsync(CancellationToken cancellationToken = default);
+
+    Task<IList<CategoryItem>> GetCategorysAsync(CancellationToken cancellationToken = default);
+
+    Task<bool> DeleleTagWithSlugAsync(string slug, CancellationToken cancellation = default);
+
+    Task<Tag> FindTagWithIdAsync(int id, CancellationToken cancellationToken = default);
+
+    Task<Category> FindCategoryWithIdAsync(int id, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteCategoryWithSlugAsync(string slug, CancellationToken cancellationToken);
+
+    Task<bool> IsCategoryExistSlugAsync(string slug, CancellationToken cancellationToken = default);
+
+    Task<Post> FindPostByPostQueryAsync(PostQuery postQuery, CancellationToken cancellationToken = default);
+
+    Task<IPagedList<Post>> GetPagedPostsAsync(PostQuery condition, int pageNumber = 1, int pageSize = 10, CancellationToken cancellationToken = default);
+
+    Task<Post> CreateOrUpdatePostAsync(Post post, IEnumerable<string> tags,CancellationToken cancellationToken = default);
+
+    // Task<bool> IsPostSlugExistedAsync(int postId, string slug, CancellationToken cancellationToken = default);
 }
