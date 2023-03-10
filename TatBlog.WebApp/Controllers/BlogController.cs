6f8b580e663e66a61928d9e79f9894feedae551e
pivokiller <2011353@dlu.edu.vn>
2023-03-11 00:46:51 +0700

    Homework Update

---
+++ TatBlog.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Core.DTO;
+using TatBlog.Services.Blogs;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+
+namespace TatBlog.WebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository) 
+        {
+            _blogRepository = blogRepository;
+        }
+
+        // Action này xử lý HTTP request đến trang chủ của
+        // ứng dụng web hoặc tìm kiếm bài viết theo từ khóa
+        public async Task<IActionResult> Index(
+            [FromQuery(Name = "k")] string keyword = null,
+            [FromQuery(Name = "p")] int pageNumber = 1, 
+            [FromQuery(Name = "ps")] int pageSize = 10)
+        {
+            // Tạo đối tượng chứa các điều kiện truy vấn
+            var postQuery = new PostQuery()
+            {
+                // Chỉ lấy những bài viết có trạng thái Published
+                PublishedOnly = true
+            };
+
+            // Truy vấn các bài viết theo điều kiện đã tạo
+            var postsList = await _blogRepository.GetPagedPostsAsync(postQuery, pageNumber, pageSize);
+
+            // Lưu lại điều kiện truy vấn để hiển thị trong View
+            ViewBag.PostQuery = postQuery;
+
+            // Truyền danh sách bài viết vào View để render ra HTML
+            return View(postsList);
+
+        }
+
+        public IActionResult About() => View();
+
+        public IActionResult Contact() => View();
+
+        public IActionResult Rss() => Content("Nội dung sẽ được cập nhật");
+    }
+}
